054b5f3519b2c3d9adab7f628491c8b9f731a41e
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-10 20:50:30 -0300

    Add Repositories

---
+++ api/src/main/java/com/eventostec/api/repositories/CouponRepository.java
@@ -0,0 +1,12 @@
+package com.eventostec.api.repositories;
+
+import com.eventostec.api.domain.cupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, UUID>{
+
+}
