cab8ec813d32cae18b02b480da3132e2d835043c
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-10 20:45:54 -0300

    Creating Entity

---
+++ api/src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,31 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String uf;
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
