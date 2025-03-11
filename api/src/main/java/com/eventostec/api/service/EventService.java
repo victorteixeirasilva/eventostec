7f0421a805ba1c13ae74cb57a6ed6ea06222d8f0
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-11 09:03:35 -0300

    Connecting to S3

---
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -0,0 +1,72 @@
+package com.eventostec.api.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+
+    public Event createEvent(EventRequestDTO data){
+        String imgUrl = null;
+
+        if (data.image() != null) {
+            imgUrl = this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+
+        return newEvent;
+
+    }
+
+    private String uploadImg(MultipartFile image) {
+        String fileName = UUID.randomUUID() + "-" + image.getOriginalFilename();
+
+        try {
+            File file = this.convertMultipartToFile(image);
+            s3Client.putObject(bucketName, fileName, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, fileName).toString();
+        } catch (Exception e) {
+            System.out.println("Erro ao subir arquivo S3");
+            return null;
+        }
+//        return "";
+    }
+
+    private File convertMultipartToFile(MultipartFile image) throws IOException {
+        File convFile = new File(Objects.requireNonNull(image.getOriginalFilename()));
+
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(image.getBytes());
+        fos.close();
+
+        return convFile;
+    }
+
+}
